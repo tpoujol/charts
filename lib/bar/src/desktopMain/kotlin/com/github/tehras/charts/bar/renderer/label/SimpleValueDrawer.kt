608731cc0b6e82ead4a70a93abe44d89c34d9355
Thomas Poujol <tpoujol@users.noreply.github.com>
2022-01-03 17:32:26 +0100

    Make bar chart functional
    - Change paint classes in label drawer to use the compose one to be as close as possible to android implem
    - Implem y axis drawer

--- lib/bar/src/desktopMain/kotlin/com/github/tehras/charts/bar/renderer/label/SimpleValueDrawer.kt
+++ lib/bar/src/desktopMain/kotlin/com/github/tehras/charts/bar/renderer/label/SimpleValueDrawer.kt
@@ -1,15 +1,11 @@
 package com.github.tehras.charts.bar.renderer.label
 
 import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.graphics.Canvas
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.TextUnit
 import com.github.tehras.charts.bar.renderer.DrawLocation
 import org.jetbrains.skia.Font
-import org.jetbrains.skia.Paint
 import org.jetbrains.skia.TextLine
 
 actual class SimpleValueDrawer actual constructor(
@@ -19,7 +15,7 @@ actual class SimpleValueDrawer actual constructor(
 ) : LabelDrawer {
     private val _labelTextArea: Float? = null
     private val paint = Paint().apply {
-        this.color = labelTextColor.toArgb()
+        this.color = labelTextColor
     }
 
 
@@ -45,22 +41,17 @@ actual class SimpleValueDrawer actual constructor(
         label: String,
         barArea: Rect,
         xAxisArea: Rect
-    ) {
-        val txLine = with(drawScope){
-            TextLine.make(label, Font(typeface = null, size = labelTextSize.toPx()))
-        }
+    ) = with(drawScope){
+        val txLine = TextLine.make(label, Font(typeface = null, size = labelTextSize.toPx()))
+
         val xCenter = barArea.left + (barArea.width / 2) - txLine.width / 2
 
-        val yCenter = with(drawScope){
-             when (drawLocation) {
-                DrawLocation.Inside -> (barArea.top + barArea.bottom) / 2
-                DrawLocation.Outside -> (barArea.top) - (labelTextSize.toPx()) / 2
-                DrawLocation.XAxis -> barArea.bottom + labelTextHeight(drawScope)
-            }
+        val yCenter = when (drawLocation) {
+            DrawLocation.Inside -> (barArea.top + barArea.bottom) / 2
+            DrawLocation.Outside -> (barArea.top) - (labelTextSize.toPx()) / 2
+            DrawLocation.XAxis -> barArea.bottom + labelTextHeight(drawScope)
         }
-
-
-
-        canvas.nativeCanvas.drawTextLine(txLine, xCenter, yCenter, paint)
+        canvas.nativeCanvas.drawTextLine(txLine, xCenter, yCenter, paint.asFrameworkPaint())
+        return@with
     }
 }
